b0c6f148b2b57debb6783246bb3184bb1fedcead
Bill Bensing <bill@billbensing.com>
2022-08-04 18:51:42 -0400

    added id value object with tests (#2)

---
+++ identity/id/id.go
@@ -0,0 +1,19 @@
+package id
+
+import "strconv"
+
+type Id struct {
+	id int64
+}
+
+func New(id int64) *Id {
+	return &Id{id: id}
+}
+
+func (id *Id) Value() string {
+	return strconv.Itoa(int(id.id))
+}
+
+func (id *Id) AsInteger() int64 {
+	return id.id
+}
