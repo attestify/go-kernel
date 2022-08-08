6bec00b433346e1886eb5496e640454fa2da1481
Bill Bensing <bill@billbensing.com>
2022-08-07 21:23:44 -0400

    removed test constraints to check for nil value if error happens

--- uri/top_level_domain/TopLevelDomain_test.go
+++ uri/top_level_domain/TopLevelDomain_test.go
@@ -21,7 +21,8 @@ func Test_InstantiateTopLevelDomain(t *testing.T) {
 
 func Test_MustReturnErrorForEmptyString(t *testing.T) {
 
-	tld, err := New("")
+
+	_, err := New("")
 
 	// Fatal use to end test if an error obejct was not returned because the expessions after this evaluate the error object
 	if err == nil {
@@ -30,9 +31,6 @@ func Test_MustReturnErrorForEmptyString(t *testing.T) {
 
 	if err.Error() != "The top level domain value must be atleast one (1) character." {
 		t.Error("The following expected error message was not returned: 'The top level domain value must be atleast one (1) character.'.")
-	}
-	if tld == nil {
-		t.Error("Expected an instantiated, empty, TopLeveLDomain object, but got a 'nil' value.")
 	}
 
 }
@@ -69,9 +67,6 @@ func Test_MustReturnErrorForNumberInString(t *testing.T) {
 	if err.Error() != "The top level domain value can only be letters." {
 		t.Error("The following expected error message was not returned: 'The top level domain value can only be letters.'.")
 	}
-	if tld == nil {
-		t.Error("Expected an instantiated, empty, TopLeveLDomain object, but got a 'nil' value.")
-	}
 
 }
 
@@ -86,9 +81,6 @@ func Test_MustReturnErrorForSymbolInString(t *testing.T) {
 
 	if err.Error() != "The top level domain value can only be letters." {
 		t.Error("The following expected error message was not returned: 'The top level domain value can only be letters.'.")
-	}
-	if tld == nil {
-		t.Error("Expected an instantiated, empty, TopLeveLDomain object, but got a 'nil' value.")
 	}
 
 }
