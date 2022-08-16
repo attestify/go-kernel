888805dc7c40304c39e093aea0af5bf60783115a
Bill Bensing <bill@billbensing.com>
2022-08-16 06:54:18 -0400

    Added revoke access gateway

---
+++ access_control/RevokeAccessGateway.go
@@ -0,0 +1,13 @@
+package access_control
+
+import "github.com/attestify/go-kernel/identity/id"
+
+// RevokeAccessGateway provides the behavior for remove an access control
+// Expected Alternative Behaviors
+//  - If the access control exists, GrantAccessGateway will remove and respond with success
+//	- If the does not exists, GrantAccessGateway will respond with success.
+// Returns one (1) type of error:
+//   - InternalError - Returned if there is an error invoking the implementation of this interface
+type RevokeAccessGateway interface {
+	Revoke(userId id.Id, entityId id.Id, entity string) error
+}
