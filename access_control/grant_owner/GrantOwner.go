8f03522e51e7bab056c95b3fcb535a955d6ade09
Bill Bensing <bill@billbensing.com>
2022-08-29 21:47:35 -0400

    added GrantOwner features and testing

--- access_control/grant_owner/GrantOwner.go
+++ access_control/grant_owner/GrantOwner.go
@@ -1,21 +1,49 @@
 package grant_owner
 
+import (
+	"github.com/attestify/go-kernel/access_control"
+	"github.com/attestify/go-kernel/error/internal_error"
+)
+
 type GrantOwner struct {
+	accessControl access_control.AccessControl
+	grantOwnerGateway GrantOwnerGateway
 	grantOwnerError error
 }
 
 func New(gateway GrantOwnerGateway) GrantOwner {
-	return GrantOwner{}
+	var gatewayError error
+	if gateway == nil {
+		gatewayError = internal_error.New("the provided ModifyAccessGateway is nil, please provide a valid instance of an ModifyAccessGateway")
+	}
+	return GrantOwner{
+		grantOwnerError: gatewayError,
+		grantOwnerGateway: gateway,
+	}
 }
 
-func (uc GrantOwner) Grant(userId int64, resourceId int64) {
-
+func (usecase *GrantOwner) Grant(userId int64, resourceId int64) {
+	usecase.setAccessControl(userId, resourceId)
+	usecase.grantOwner()
 }
 
-func (uc GrantOwner) HasError() bool {
-	return uc.grantOwnerError != nil
+func (usecase *GrantOwner) setAccessControl(userId int64, resourceId int64) {
+	if usecase.HasError() { return }
+	usecase.accessControl = access_control.New(userId, resourceId, []string{})
 }
 
-func (uc GrantOwner) Error() error {
-	return uc.grantOwnerError
+func (usecase *GrantOwner) grantOwner() {
+	if usecase.HasError() { return }
+	usecase.grantOwnerGateway.Grant(usecase.accessControl)
+	if usecase.grantOwnerGateway.HasError() {
+		usecase.grantOwnerError = usecase.grantOwnerGateway.Error()
+	}
+}
+
+func (usecase GrantOwner) HasError() bool {
+	return usecase.grantOwnerError != nil
+}
+
+func (usecase GrantOwner) Error() error {
+	return usecase.grantOwnerError
 }
--- access_control/grant_owner/GrantOwner_test.go
+++ access_control/grant_owner/GrantOwner_test.go
@@ -2,6 +2,7 @@ package grant_owner_test
 
 import (
 	"errors"
+	"github.com/attestify/go-kernel/access_control"
 	"github.com/attestify/go-kernel/access_control/grant_owner"
 	"github.com/attestify/go-kernel/error/internal_error"
 	"testing"
@@ -22,7 +23,7 @@ func Test_Instantiate_GrantOwner_Successfully(t *testing.T) {
 	gateway := NewGrantOwnerGatewayMock()
 
 	//Act
-	usecase := grant_owner.New(gateway)
+	usecase := grant_owner.New(&gateway)
 
 	//Assert
 	if usecase.HasError() {
@@ -31,27 +32,29 @@ func Test_Instantiate_GrantOwner_Successfully(t *testing.T) {
 
 }
 
-//func Test_Invoke_Grant_Successfully(t *testing.T) {
-//	setup(t)
-//	// Assemble
-//	gateway := NewGrantOwnerGatewayMock()
-//	usecase := grant_owner.New(gateway)
-//	var userId int64 = 0
-//	var resourceId int64 = 1
-//
-//	//Act
-//	usecase.Grant(userId, resourceId)
-//
-//	//Assert
-//	if usecase.HasError() {
-//		t.Errorf("An unexpected error occurred.\n Error: %s\n", usecase.Error())
-//	}
-//
-//}
+// Given the GrantOwner usecase is instantiated without error
+// When the .Grant(..) is invoked with a userId of 0 and a resourceId of 1
+// Then there should not be any errors
+func Test_Invoke_Grant_Successfully(t *testing.T) {
+	setup(t)
+	// Assemble
+	gateway := NewGrantOwnerGatewayMock()
+	var userId int64 = 0
+	var resourceId int64 = 1
+
+	//Act
+	usecase := grant_owner.New(&gateway)
+	usecase.Grant(userId, resourceId)
+
+	//Assert
+	if usecase.HasError() {
+		t.Errorf("An unexpected error occurred.\n Error: %s\n", usecase.Error())
+	}
+
+}
 
 /** Sad Path **/
 
-// todo - Left off here - need to complete implementation to fulfill the test
 // Given there is a valid GrantOwnerGateway dependency
 // When the GrantOwner usecase is instantiated
 // Then there should be an error
@@ -65,7 +68,7 @@ func Test_Instantiate_GrantOwner_With_Bad_GrantOwnerGateway(t *testing.T) {
 	usecase := grant_owner.New(gateway)
 
 	//Assert
-	if !usecase.HasError() {
+	if usecase.HasError() == false {
 		t.Fatal("An error was expected, but no error was returned")
 	}
 
@@ -81,12 +84,60 @@ func Test_Instantiate_GrantOwner_With_Bad_GrantOwnerGateway(t *testing.T) {
 
 }
 
+// Given the GrantOwner usecase is instantiated without error
+// When the .Grant(..) is invoked with a userId of 0 and a resourceId of 1
+//	And the GrantOwnerGateway occurs an InternalError
+// Then the GrantOwner usecase should reflect InternalError
+func Test_Handle_InternalError_Invoking_GrantOwnerGateway(t *testing.T) {
+	setup(t)
+	// Assemble
+	gateway := NewGrantOwnerGatewayMock()
+	gateway.GenerateInternalError()
+	usecase := grant_owner.New(&gateway)
+	if usecase.HasError() == true {
+		t.Fatalf("An error was thrown when non was expected.\n Error: %s\n", usecase.Error())
+	}
 
+	//Act
+	var userId int64 = 0
+	var resourceId int64 = 1
+	usecase.Grant(userId, resourceId)
+
+	//Assert
+	if usecase.HasError() != true {
+		t.Fatal("An error was expected, but no error was returned")
+	}
+
+	if !errors.As(usecase.Error(), &internal_error.InternalError{}) {
+		t.Errorf("did not get the epected error of type InternalError")
+	}
+
+}
+
+// todo - test gate for setAccessControl
+// todo - test gate for grantOwner
 
 /** Testing Tools **/
 
-type GrantOwnerGatewayMock struct {}
+type GrantOwnerGatewayMock struct {
+	generateInternalError bool
+	mockError error
+}
 
 func NewGrantOwnerGatewayMock()  GrantOwnerGatewayMock {
 	return GrantOwnerGatewayMock{}
+}
+
+func (mock *GrantOwnerGatewayMock) Grant(accessControl access_control.AccessControl)  {}
+
+func (mock GrantOwnerGatewayMock) HasError() bool {
+	return mock.mockError != nil
+}
+
+func (mock GrantOwnerGatewayMock) Error() error {
+	return mock.mockError
+}
+
+func (mock *GrantOwnerGatewayMock) GenerateInternalError() {
+	mock.mockError = internal_error.New("Internal error when .Grant(...) was invoked")
 }
