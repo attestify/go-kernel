f800703fb1cfa353945093f883f0b86c9dc14750
Bill Bensing <bill@billbensing.com>
2022-08-31 08:58:46 -0400

    Updated tests to be consistent with eachother

--- access_control/grant_owner/GrantOwner_test.go
+++ access_control/grant_owner/GrantOwner_test.go
@@ -20,10 +20,10 @@ func setup(t *testing.T) {
 func Test_Instantiate_GrantOwner_Successfully(t *testing.T) {
 	setup(t)
 	// Assemble
-	gateway := NewGrantOwnerGatewayMock()
+	gateway := NewMockGrantOwnerGateway()
 
 	//Act
-	usecase := grant_owner.New(gateway)
+	usecase := grant_owner.New(&gateway)
 
 	//Assert
 	if usecase.HasError() {
@@ -38,12 +38,12 @@ func Test_Instantiate_GrantOwner_Successfully(t *testing.T) {
 func Test_Invoke_Grant_Successfully(t *testing.T) {
 	setup(t)
 	// Assemble
-	gateway := NewGrantOwnerGatewayMock()
+	gateway := NewMockGrantOwnerGateway()
 	var userId int64 = 0
 	var resourceId int64 = 1
 
 	//Act
-	usecase := grant_owner.New(gateway)
+	usecase := grant_owner.New(&gateway)
 	usecase.Grant(userId, resourceId)
 
 	//Assert
@@ -55,12 +55,15 @@ func Test_Invoke_Grant_Successfully(t *testing.T) {
 
 /** Sad Path **/
 
-// Given there is a valid GrantOwnerGateway dependency
+/** Side Effects of nil GrantOwnerGateway **/
+
+// Given there is a nil GrantOwnerGateway dependency
 // When the GrantOwner usecase is instantiated
-// Then there should be an error
-//  with the error message "the provided GrantOwnerGateway is nil, please provide a valid instance of an GrantOwnerGateway"
-func Test_Instantiate_GrantOwner_With_Bad_GrantOwnerGateway(t *testing.T) {
+// Then there should be an InternalError with the error message:
+//   "the provided GrantOwnerGateway is nil, please provide a valid instance of an GrantOwnerGateway"
+func Test_Handle_InternalError_With_Nil_GrantOwnerGateway_When_Instantiated(t *testing.T) {
 	setup(t)
+
 	// Assemble
 	var gateway grant_owner.GrantOwnerGateway = nil
 
@@ -76,6 +79,7 @@ func Test_Instantiate_GrantOwner_With_Bad_GrantOwnerGateway(t *testing.T) {
 		t.Errorf("did not get the epected error of type InternalError")
 	}
 
+	// This assertion ensure the InternalError is generated from the nil gateway check
 	actualMessage := usecase.Error().Error()
 	expectedMessage := "the provided ModifyAccessGateway is nil, please provide a valid instance of an ModifyAccessGateway"
 	if expectedMessage != actualMessage {
@@ -84,39 +88,11 @@ func Test_Instantiate_GrantOwner_With_Bad_GrantOwnerGateway(t *testing.T) {
 
 }
 
-// Given the GrantOwner usecase is instantiated without error
-// When the .Grant(..) is invoked with a userId of 0 and a resourceId of 1
-//	And the GrantOwnerGateway occurs an InternalError
-// Then the GrantOwner usecase should reflect InternalError
-func Test_Handle_InternalError_Invoking_GrantOwnerGateway(t *testing.T) {
-	setup(t)
-	// Assemble
-	gateway := NewGrantOwnerGatewayMock()
-	gateway.GenerateInternalError()
-	usecase := grant_owner.New(gateway)
-	if usecase.HasError() == true {
-		t.Fatalf("An error was thrown when non was expected.\n Error: %s\n", usecase.Error())
-	}
-
-	//Act
-	var userId int64 = 0
-	var resourceId int64 = 1
-	usecase.Grant(userId, resourceId)
-
-	//Assert
-	if usecase.HasError() != true {
-		t.Fatal("An error was expected, but no error was returned")
-	}
-
-	if !errors.As(usecase.Error(), &internal_error.InternalError{}) {
-		t.Errorf("did not get the epected error of type InternalError")
-	}
-
-}
-
+// NOTE - this tests that error guards function properly
 // Given the GrantOwner usecase is instantiated with a nil GrantOwnerGateway dependency
-// When the GrantOwner.Grant(...) is invoked
-// Then there should be an error InternalError
+// When the GrantOwner.Grant(...) is invoked with valid parameters
+// Then there should be an InternalError with the error message:
+//   "the provided GrantOwnerGateway is nil, please provide a valid instance of an GrantOwnerGateway"
 func Test_Returns_InternalError_With_Nil_GrantOwnerGateway_When_Grant_Invoked(t *testing.T) {
 	setup(t)
 	// Assemble
@@ -137,34 +113,83 @@ func Test_Returns_InternalError_With_Nil_GrantOwnerGateway_When_Grant_Invoked(t 
 		t.Errorf("did not get the epected error of type InternalError")
 	}
 
+	// This assertion ensure the InternalError is generated from the nil gateway check
+	actualMessage := usecase.Error().Error()
+	expectedMessage := "the provided ModifyAccessGateway is nil, please provide a valid instance of an ModifyAccessGateway"
+	if expectedMessage != actualMessage {
+		t.Errorf("The returned error message was not expected: \n Expected: %s \n Actual %s", expectedMessage, actualMessage)
+	}
+
+}
+
+/** Side Effects of GrantAllGateway errors **/
+
+// Given the GrantOwner usecase is instantiated without error
+// When .Grant(...) is invoked with valid parameters
+// Then  the GrantOwnerGateway returns an InternalError
+//  and the GrantOwner usecase should reflect InternalError returned by the GrantOwnerGateway
+func Test_Handle_GrantOwnerGateway_Returns_InternalError_When_Grant_Invoked(t *testing.T) {
+	setup(t)
+
+	// Assemble
+	gateway := NewMockGrantOwnerGateway()
+	gateway.GrantOwnerGatewayInternalError()
+	usecase := grant_owner.New(&gateway)
+	if usecase.HasError() == true {
+		t.Fatalf("An error was thrown when non was expected.\n Error: %s\n", usecase.Error())
+	}
+
+	//Act
+	var userId int64 = 0
+	var resourceId int64 = 1
+	usecase.Grant(userId, resourceId)
+
+	//Assert
+	if usecase.HasError() != true {
+		t.Fatal("An error was expected, but no error was returned")
+	}
+
+	if !errors.As(usecase.Error(), &internal_error.InternalError{}) {
+		t.Errorf("did not get the epected error of type InternalError")
+	}
+
+	// This assertion ensure the InternalError is generate when the GrantOwner.Grant(...) method is invoked
+	// If the message is other than what's here, the InternalError was generated by something else than .Grant(...)
+	// By testing this, we can expect that and InternalError generated  by any other implementation of the
+	// GrantOwnerGateway will be properly handled
+	actualMessage := usecase.Error().Error()
+	expectedMessage := "Error generated from GrantOwner.Grant() invocation."
+	if expectedMessage != actualMessage {
+		t.Errorf("The returned error message was not expected: \n Expected: %s \n Actual %s", expectedMessage, actualMessage)
+	}
+
 }
 
 /** Testing Tools **/
 
-type GrantOwnerGatewayMock struct {
-	accessControl         access_control.AccessControl
-	generateInternalError bool
-	mockError             error
+type MockGrantOwnerGateway struct {
+	grantOwnerGatewayInternalError bool
+	mockError                      error
 }
 
-func NewGrantOwnerGatewayMock() GrantOwnerGatewayMock {
-	return GrantOwnerGatewayMock{}
+func NewMockGrantOwnerGateway() MockGrantOwnerGateway {
+	return MockGrantOwnerGateway{}
 }
 
-func (mock GrantOwnerGatewayMock) Grant(accessControl access_control.AccessControl) {}
-
-func (mock GrantOwnerGatewayMock) HasError() bool {
-	return mock.mockError != nil
+func (gateway* MockGrantOwnerGateway) Grant(accessControl access_control.AccessControl) {
+	if gateway.grantOwnerGatewayInternalError {
+		gateway.mockError =  internal_error.New("Error generated from GrantOwner.Grant() invocation.")
+	}
 }
 
-func (mock GrantOwnerGatewayMock) Error() error {
-	return mock.mockError
+func (gateway *MockGrantOwnerGateway) GrantOwnerGatewayInternalError() {
+	gateway.grantOwnerGatewayInternalError = true
 }
 
-func (mock *GrantOwnerGatewayMock) GenerateInternalError() {
-	mock.mockError = internal_error.New("Internal error when .Grant(...) was invoked")
+func (gateway MockGrantOwnerGateway) HasError() bool {
+	return gateway.mockError != nil
 }
 
-func (mock GrantOwnerGatewayMock) GetAccessControl() access_control.AccessControl {
-	return mock.accessControl
+func (gateway MockGrantOwnerGateway) Error() error {
+	return gateway.mockError
 }
